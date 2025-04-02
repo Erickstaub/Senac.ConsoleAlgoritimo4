18fd702fba69bba5db693a4d88e58e9a434cacd6
Erick Staub <erickstaubfelipe@gmail.com>
2025-04-01 21:09:50 -0300

    1.1 beta upd

--- algoritimo4/algoritimo4/Program.cs
+++ algoritimo4/algoritimo4/Program.cs
@@ -1,3 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+using System.Globalization;
+
+    Console.WriteLine("Quanto vai gastar na reforma?");
+    Console.WriteLine();
+
+//largura da parede
+    Console.WriteLine("Qual largura");
+    Console.WriteLine();
+    double largura;
+        largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//comprimento da parede
+    Console.WriteLine("Qual comprimento");
+    Console.WriteLine();
+    double comprimento;
+        comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//arae da parede
+    Console.WriteLine("Qual area");
+    Console.WriteLine();
+    double area;
+        area = largura * comprimento ;
+    Console.Write(area + "m²");
+    Console.WriteLine();
+
+//preco do m²
+    Console.WriteLine("Quanto custa o m²");
+    Console.WriteLine();
+    double preco;
+        preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//todal do preço
+    double total;
+        total = area * preco ;
+    Console.Write(total.ToString("F2") + "R$");
